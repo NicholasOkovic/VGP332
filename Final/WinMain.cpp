918544eddf91de087596a2e0d132817c411b730a
NicholasOkovic <139954443+NicholasOkovic@users.noreply.github.com>
2024-03-14 16:44:12 -0700

    started on final

---
+++ Final/WinMain.cpp
@@ -0,0 +1,103 @@
+#include "TileMap.h"
+#include <XEngine.h> // <> for external includes, "" for internal includes
+#include <ImGui/Inc/imgui.h>
+
+TileMap tileMap;
+X::TextureId textureId;
+X::Math::Vector2 position;
+Path path;
+int startX = 5;
+int startY = 9;
+int endX = 20;
+int endY = 12;
+
+
+//--------------------------------------------------
+
+void GameInit()
+{
+	tileMap.LoadTiles("tiles.txt");
+	tileMap.LoadMap("map.txt");
+
+	textureId = X::LoadTexture("bird1.png");
+	position = { 100.0f, 100.0f };
+}
+
+bool GameLoop(float deltaTime)
+{
+	const float moveSpeed = 200.0f; // pixel per second
+	if (X::IsKeyDown(X::Keys::RIGHT))
+		position.x += moveSpeed * deltaTime;
+	else if (X::IsKeyDown(X::Keys::LEFT))
+		position.x -= moveSpeed * deltaTime;
+	if (X::IsKeyDown(X::Keys::DOWN))
+		position.y += moveSpeed * deltaTime;
+	else if (X::IsKeyDown(X::Keys::UP))
+		position.y -= moveSpeed * deltaTime;
+
+	ImGui::Begin("PathFinding", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+	{
+		const int columns = tileMap.GetColumns();
+		const int rows = tileMap.GetRows();
+		ImGui::DragInt("StartX", &startX, 1, 0, columns - 1);
+		ImGui::DragInt("StartY", &startY, 1, 0, rows - 1);
+		ImGui::DragInt("EndX", &endX, 1, 0, columns - 1);
+		ImGui::DragInt("EndY", &endY, 1, 0, rows - 1);
+		if (ImGui::Button("RunBFS"))
+		{
+			path = tileMap.FindPathBFS(startX, startY, endX, endY);
+		}
+		if (ImGui::Button("RunDFS"))
+		{
+			path = tileMap.FindPathDFS(startX, startY, endX, endY);
+		}
+		if (ImGui::Button("RunDijkstra"))
+		{
+			path = tileMap.FindPathDijkstra(startX, startY, endX, endY);
+		}
+		if (ImGui::Button("RunAStar"))
+		{
+			path = tileMap.FindPathAStar(startX, startY, endX, endY);
+		}
+
+		int test = 0;
+		ImGui::DragInt("TestValue", &test);
+		ImGui::Button("Press");
+	}
+	ImGui::End();
+
+	tileMap.Render();
+
+	X::DrawSprite(textureId, position, X::Pivot::TopLeft, X::Flip::Horizontal);
+	X::DrawScreenDiamond(position, 5.0f, X::Colors::Cyan);
+
+	for (int i = 1; i < path.size(); ++i)
+	{
+		X::DrawScreenLine(path[i - 1], path[i], X::Colors::Red);
+	}
+	X::DrawScreenCircle(tileMap.GetPixelPosition(startX, startY), startY, X::Colors::Pink);
+	X::DrawScreenCircle(tileMap.GetPixelPosition(endX, endY), endY, X::Colors::Yellow);
+
+
+	const bool quit = X::IsKeyPressed(X::Keys::ESCAPE);
+	return quit;
+}
+
+void GameCleanup()
+{
+
+}
+
+//--------------------------------------------------
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	X::Start("Pathfinding");
+	GameInit();
+
+	X::Run(GameLoop);
+
+	GameCleanup();
+	X::Stop();
+	return 0;
+}
